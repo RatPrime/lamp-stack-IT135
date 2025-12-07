e1c9c287decf9482a51b2b471bbf3d3d0e9df65b
RatPrime <38698839+RatPrime@users.noreply.github.com>
2025-12-06 18:33:32 -0800

    Update lamp_setup.sh

--- lamp_setup.sh
+++ lamp_setup.sh
@@ -14,3 +14,11 @@ sudo ln -s "$REPO_PATH" /var/www/html
 sudo chmod -R 755 "$REPO_PATH"
 
 echo "Symlink created: $REPO_PATH -> /var/www/html"
+
+# Runs the db_setup.sql script to set up the database.
+sudo service mariadb start
+echo "MariaDB started"
+sudo mariadb -u root < db_setup.sql
+echo "Database loaded"
+sudo service apache2 start
+echo "Apache started"
